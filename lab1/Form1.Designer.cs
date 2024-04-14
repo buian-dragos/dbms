4881e2cfb8189b486b585940375a537393c0db19
Buian Dragos <115634138+buian-dragos@users.noreply.github.com>
2024-04-15 02:39:12 +0300

    Add files via upload

---
+++ lab1/Form1.Designer.cs
@@ -0,0 +1,115 @@
+﻿namespace lab1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(45, 66);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.Size = new System.Drawing.Size(500, 400);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Location = new System.Drawing.Point(568, 66);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.RowHeadersWidth = 51;
+            this.dataGridView2.Size = new System.Drawing.Size(700, 400);
+            this.dataGridView2.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(45, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(64, 16);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Customer";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(565, 27);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(41, 16);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Order";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(506, 500);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 35);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Update";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1282, 753);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridView2);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ lab1/Form1.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Configuration;
+
+
+namespace lab1
+{
+    public partial class Form1 : Form
+    {
+        SqlConnection conn;
+        SqlDataAdapter daOrder;
+        SqlDataAdapter daCustomer;
+        DataSet dset;
+        BindingSource bsOrder;
+        BindingSource bsCustomer;
+
+        SqlCommandBuilder cmdBuilder1;
+        SqlCommandBuilder cmdBuilder2;
+
+        string queryOrder;
+        string queryCustomer;
+
+        string table1;
+        string table2;
+
+        public Form1()
+        {
+            InitializeComponent();
+            FillData();
+        }
+
+        void FillData()
+        {
+            conn = new SqlConnection(getConnectionString());
+
+            table1 = ConfigurationManager.AppSettings["table1"];
+            table2 = ConfigurationManager.AppSettings["table2"];
+
+            queryCustomer = "SELECT * FROM Customer";
+            queryOrder = "SELECT * FROM Orders";
+
+            daCustomer = new SqlDataAdapter(queryCustomer, conn);
+            daOrder = new SqlDataAdapter(queryOrder, conn);
+
+            dset = new DataSet();
+
+            daCustomer.Fill(dset, "Customer");
+            daOrder.Fill(dset, "Orders");
+
+            cmdBuilder1 = new SqlCommandBuilder(daCustomer);
+            cmdBuilder2 = new SqlCommandBuilder(daOrder);
+
+
+            dset.Relations.Add("CustomerOrder",
+                dset.Tables["Customer"].Columns["customer_id"],
+                dset.Tables["Orders"].Columns["customer_id"]);
+            /*
+            this.dataGridView1.DataSource = dset.Tables["Customer"];
+            this.dataGridView2.DataSource = this.dataGridView1.DataSource;
+            this.dataGridView2.DataMember = "CustomerOrder";
+            */
+
+            bsCustomer = new BindingSource();
+            bsCustomer.DataSource = dset.Tables["Customer"];
+            bsOrder = new BindingSource(bsCustomer, "CustomerOrder");
+
+            this.dataGridView1.DataSource = bsCustomer;
+            this.dataGridView2.DataSource = bsOrder;
+
+
+            cmdBuilder1.GetUpdateCommand();
+            cmdBuilder2.GetUpdateCommand();
+        }
+
+        string getConnectionString()
+        {
+            return "Data Source=DBSB\\MS_SQLSERVER;Initial Catalog=Furniture Shop;" +
+                "Integrated Security=true;";
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            /*MessageBox.Show(ConfigurationManager.AppSettings["greeting"]);*/
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            daCustomer.Update(dset, "Customer");
+            daOrder.Update(dset, "Orders");
+        }
+    }
+}
